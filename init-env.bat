bedcb3b8a0857a631c6f262759a6fb1e0e116462
greshje <greshje@gmail.com>
2024-05-10 08:07:58 -0400

    work in progress (tests passing)

--- init-env.bat
+++ init-env.bat
@@ -3,3 +3,23 @@
 ::
 
 python -m venv venv
+./venv/Scripts/activate
+pip install dbt-core
+pip install dbt-postgres
+dbt --version
+
+::
+:: The following is run once to create the project
+::
+
+dbt init DBT_DEMO
+
+::
+:: Note: Database connection parameters are stored in C:\Users\me\.dbt\profiles.yml
+::
+
+cd DBT_DEMO
+dbt debug
+
+
+
